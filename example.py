8ed8cb8f42596ffb6172f7d7596ce00e130cc402
Zado <zado.keren@gmail.com>
2023-04-16 19:53:57 +0700

    mengimplementasikan numpy dan menghapus emoji pada covert ke csv

--- example.py
+++ example.py
@@ -1,19 +1,24 @@
 import snscrape.modules.twitter as sntwitter
+import numpy as np
 import pandas as pd
 
-query = "(from:elonmusk) until:2020-01-01 since:2010-01-01"
-tweets = []
+query = "(to:puanmaharani_ri) until:2023-04-16 since:2022-04-16 filter:replies"
 limit = 200
 
+tweets = np.empty((limit, 3), dtype=object)
+index = 0
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-    
-    # print(vars(tweet))
-    # break
-    if len(tweets) == limit:
+    if index == limit:
         break
     else:
-        tweets.append([tweet.date, tweet.username, tweet.content])
-        
+        tweets[index, 0] = tweet.date
+        tweets[index, 1] = tweet.username
+        tweets[index, 2] = tweet.content
+        index += 1
+
+tweets = tweets[:index, :]  # menghapus baris yang tidak terisi pada array
+
 df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
-df.to_csv("hasil.csv",sep=";")
+df['Tweet'] = df['Tweet'].apply(lambda x: x.encode('ascii', 'ignore').decode('ascii'))
+df.to_csv("hasil.csv", sep=";", encoding="utf-8", index=False)
